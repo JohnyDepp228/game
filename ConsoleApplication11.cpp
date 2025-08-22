5ac51efeee216100920ad35aae66ba58482cb21f
Atriom <artemmasuk722@gmail.com>
2025-08-23 00:51:54 +0300

    убрать создание массива

--- ConsoleApplication11.cpp
+++ ConsoleApplication11.cpp
@@ -14,9 +14,24 @@ void character(int a, int b);
 int main()
 {
     setlocale(LC_ALL, "RU");
-    int(*ptr)() = type;
-    cout << ptr();
-    //map(40, 8,ptr);
+    srand(time(0));
+    int prev1 = 0;
+    int res = type();
+    int arrRes[4];
+    for (int i = 0; i < 4; i++) {
+        res = type();
+        while (res == prev1) {
+            res = type();
+        }
+        arrRes[i] = res;
+        prev1 = res;
+    }
+    map(20, 5, arrRes[1]);
+    cout << arrRes[1] << endl;
+    cout << arrRes[0] << endl;
+    cout << arrRes[2];
+    //cout << res << endl;
+    // сделать массив хранящий res и его передавать в map для построения карты 
     //character(5, 5);
     return 0;
 }
