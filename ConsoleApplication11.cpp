2b962d5aa2e39a50e4447f12e981dc38a10ad092
Atriom <artemmasuk722@gmail.com>
2025-08-25 00:29:06 +0300

    ничего не менять

--- ConsoleApplication11.cpp
+++ ConsoleApplication11.cpp
@@ -15,24 +15,7 @@ int main()
 {
     setlocale(LC_ALL, "RU");
     srand(time(0));
-    int prev1 = 0;
-    int res = type();
-    int arrRes[4];
-    for (int i = 0; i < 4; i++) {
-        res = type();
-        while (res == prev1) {
-            res = type();
-        }
-        arrRes[i] = res;
-        prev1 = res;
-    }
-    map(20, 5, arrRes[1]);
-    cout << arrRes[1] << endl;
-    cout << arrRes[0] << endl;
-    cout << arrRes[2];
-    //cout << res << endl;
-    // сделать массив хранящий res и его передавать в map для построения карты 
-    //character(5, 5);
+    character(1, 9);
     return 0;
 }
 
